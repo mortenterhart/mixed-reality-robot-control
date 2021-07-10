50a6fbbb7f08a5cebe3a0a6c55eb0e63572885a2
Morten Terhart <morten.terhart@gmail.com>
2021-07-10 17:41:20 +0200

    Add button events and speech commands

---
+++ Assets/Scripts/RobotCommandActions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RobotCommandActions : MonoBehaviour
+{
+    public void StoreItem(int shelfId)
+    {
+        Debug.Log($"Store {shelfId}");
+    }
+
+    public void LoadItem(int shelfId)
+    {
+        Debug.Log($"Load {shelfId}");
+    }
+}
+    
