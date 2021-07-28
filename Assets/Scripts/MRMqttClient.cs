264b098fc8585a83c089e0bb5b14da1385353d37
Morten Terhart <morten.terhart@gmail.com>
2021-07-28 18:47:55 +0200

    Add MQTT client for sending commands to robot

---
+++ Assets/Scripts/MRMqttClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using M2MqttUnity;
+
+public class MRMqttClient : M2MqttUnityClient
+{
+    private const string COMMANDS_TOPIC = "commands"; 
+    
+    public void SendStoreIn(int shelfId)
+    {
+        var command = Encoding.UTF8.GetBytes($"si{shelfId}");
+        client.Publish(COMMANDS_TOPIC, command);
+    }
+    
+    public void SendStoreOut(int shelfId)
+    {
+        var command = Encoding.UTF8.GetBytes($"so{shelfId}");
+        client.Publish(COMMANDS_TOPIC, command);
+    }
+
+    private void OnDestroy()
+    {
+        Disconnect();
+    }
+}
---
+++ Assets/Scripts/RobotCommands.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class RobotCommands : MonoBehaviour
+{
+    private int shelfId = -1;
+    private MRMqttClient client;
+
+    private void Awake()
+    {
+        client = GetComponent<MRMqttClient>();
+    }
+
+    public void StoreItem()
+    {
+        Debug.Log($"Store to shelf {shelfId}");
+
+        if (shelfId != -1)
+        {
+            client.SendStoreIn(shelfId);
+        }
+    }
+
+    public void LoadItem()
+    {
+        Debug.Log($"Load from shelf {shelfId}");
+
+        if (shelfId != -1)
+        {
+            client.SendStoreOut(shelfId);
+        }
+    }
+
+    public void SelectShelf(int shelfId)
+    {
+        Debug.Log($"Selecting shelf {shelfId}");
+        this.shelfId = shelfId;
+    }
+}
+    
