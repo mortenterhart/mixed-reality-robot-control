600e5d1a8189150749fac8b6f54a390aaf72b6b4
Morten Terhart <morten.terhart@gmail.com>
2021-07-29 01:13:10 +0200

    Show shelf status with colors and selected shelf

--- Assets/Scripts/RobotCommands.cs
+++ Assets/Scripts/RobotCommands.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.MixedReality.Toolkit.UI;
+﻿using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
 using UnityEngine;
 
 public class RobotCommands : MonoBehaviour
 {
+    private static readonly Color ColorFreeShelf = new Color(0.11f, 0.75f, 0f);
+    private static readonly Color ColorOccupiedShelf = new Color(0.9f, 0f, 0f);
+    
     [SerializeField] private GameObject smallDialogPrefab;
+
+    [SerializeField] private List<GameObject> shelfButtonQuads;
+    [SerializeField] private TextMeshPro selectedShelfText;
 
     private int shelfId = -1;
     private MRMqttClient client;
 
-    private void Awake()
+    private void Start()
     {
         client = GetComponent<MRMqttClient>();
+        selectedShelfText.text = "";
     }
 
     public void StoreItem()
@@ -22,6 +31,7 @@ public class RobotCommands : MonoBehaviour
         }
 
         Debug.Log($"Store to shelf {shelfId}");
+        SetShelfButtonQuadColor(ColorOccupiedShelf);
         client.SendStoreIn(shelfId);
     }
 
@@ -34,7 +44,16 @@ public class RobotCommands : MonoBehaviour
         }
         
         Debug.Log($"Load from shelf {shelfId}");
+        SetShelfButtonQuadColor(ColorFreeShelf);
         client.SendStoreOut(shelfId);
+    }
+    
+    public void SelectShelf(int shelfId)
+    {
+        Debug.Log($"Selecting shelf {shelfId}");
+        this.shelfId = shelfId;
+
+        selectedShelfText.text = $"{shelfId}";
     }
 
     private void ShowSelectShelfDialog()
@@ -42,10 +61,10 @@ public class RobotCommands : MonoBehaviour
         Dialog.Open(smallDialogPrefab, DialogButtonType.OK, "Notice", "Please select a shelf before using this command.", true);
     }
 
-    public void SelectShelf(int shelfId)
+    private void SetShelfButtonQuadColor(Color color)
     {
-        Debug.Log($"Selecting shelf {shelfId}");
-        this.shelfId = shelfId;
+        var quadRenderer = shelfButtonQuads[shelfId - 1].GetComponent<Renderer>();
+        quadRenderer.material.color = color;
     }
 }
     
